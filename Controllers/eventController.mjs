e5983b68cd34aa6ac7d808fd9fe0868f192b8be4
David Cronin <d_croni@ENCS.concordia.ca>
2025-10-06 12:42:48 -0400

    add event controller with CRUD operations for events

---
+++ Controllers/eventController.mjs
@@ -0,0 +1,47 @@
+import event from "../models/event.mjs"
+
+
+const addEvent = (req, res) => {
+    const body = req.body
+    console.log(body)
+    // Logic to add event
+    const title = body.title
+    const icon = body.icon
+    const description = body.description
+    const price = body.price
+    const capacity = body.capacity
+    const date = body.date
+    const location = body.location
+    const org_id = body.org_id
+
+    event.addEvent(title, icon, description, price, capacity, date, location, org_id)
+
+    res.status(201).json({ message : "Event added" })
+}
+
+const deleteEventById = (req, res) => {
+    const id = parseInt(req.params.id)
+    event.deleteEventById(id)
+    res.status(200).json({ message: "Event deleted" })
+}
+
+const getEventById = (req, res) => {
+    const id = parseInt(req.params.id)
+    const event = event.getEventById(id)
+    if (event) {
+        res.status(200).json(event)
+    } else {
+        res.status(404).json({ message: "Event not found" })
+    }
+}
+
+const getAllEvents = (req, res) => {
+    const events = event.getAllEvents()
+    if (events) {
+        res.status(200).json(events)
+    } else {
+        res.status(404).json({ message: "No events found" })
+    }
+}
+
+export default { addEvent, getEventById, getAllEvents, deleteEventById }
