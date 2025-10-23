2a966fd50b6569a6a7a90b97c78e4aea30015134
rima1881 <amd.gharibipour@outlook.com>
2025-10-23 15:39:04 -0400

    added file access

--- utils/sendVerificationEmail.mjs
+++ utils/sendVerificationEmail.mjs
@@ -1,4 +1,4 @@
-const nodemailer = require('nodemailer');
+import nodemailer from "nodemailer"
 
 const sendVerificationEmail = async (email, link) => {
     const transporter = nodemailer.createTransport({
